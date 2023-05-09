fb48389af1262f9726ad51d79967721cf5171108
orivera04 <riveramoreira04@gmail.com>
2023-05-09 08:24:47 -0600

    feat: navbar finished.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { Separator } from './Separator';
 
 function NavBar() {
   return (
@@ -9,17 +10,31 @@ function NavBar() {
 
       <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
         <li>
-          <NavLink to="/reminders" activeClassName="text-blue-600" className="text-sm text-gray-400 hover:text-gray-500">Reminders</NavLink>
+          <NavLink
+            to="/reminders"
+            className={({ isActive }) =>
+              isActive
+                ? 'text-blue-600 text-sm hover:text-red-400'
+                : 'text-gray-400 text-sm hover:text-red-400'
+            }
+          >
+            Reminders
+          </NavLink>
         </li>
 
-        <li className="text-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
-          </svg>
-        </li>
+        <Separator />
 
         <li>
-          <NavLink to="/settings" className="text-sm text-gray-400 hover:text-gray-500">Settings</NavLink>
+          <NavLink
+            to="/settings"
+            className={({ isActive }) =>
+              isActive
+                ? 'text-blue-600 text-sm hover:text-red-400'
+                : 'text-gray-400 text-sm hover:text-red-400'
+            }
+          >
+            Settings
+          </NavLink>
         </li>
       </ul>
     </nav>
