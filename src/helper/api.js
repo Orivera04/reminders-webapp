268948a08db03730115a456f7e8774109221fa7d
orivera04 <riveramoreira04@gmail.com>
2024-02-03 21:24:36 -0600

    Feat: Test CI.

--- src/helper/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import { getEnvironments } from './getEnviroments';
-
-const { VITE_REMINDER_SERVER_URL } = getEnvironments();
-
-export const api = axios.create({ baseURL: VITE_REMINDER_SERVER_URL });
